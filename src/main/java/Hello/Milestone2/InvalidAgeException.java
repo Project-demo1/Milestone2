49ec96c8dc5b8f2f8745375ca34f5e42a93aef52
shefali <shefalisandlya4321@gmail.com>
2022-08-05 11:25:16 +0530

    First commit

---
+++ src/main/java/Hello/Milestone2/InvalidAgeException.java
@@ -0,0 +1,12 @@
+package Hello.Milestone2;
+
+public class InvalidAgeException extends Exception  
+{  
+    public InvalidAgeException (String str)  
+    {  
+        
+        super(str);  
+    }  
+	
+
+}
